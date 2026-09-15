a15241a4220749f4193e96605451ebe310176aac
ananyameghraj <ananya9anu9@gmail.com>
2026-09-15 22:12:57 +0530

    updated main

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="CyberPredict AI API", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
